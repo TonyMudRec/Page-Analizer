0fd2cf6ea502f9e4c05d2f0d9ca8d213adc57f12
mudrec <sm1le88man@gmail.com>
2023-09-01 12:29:36 +0300

    init web service to Render

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,12 +3,20 @@
  */
 package hexlet.code;
 
+import io.javalin.Javalin;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        Javalin app = getApp();
+        app.start(8080);
+    }
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        return app;
     }
 }
